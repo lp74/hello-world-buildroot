c370a06b2060b2ccc9bf38aeb116a6aa662e9cee
Luca Polverini <luca.polverini@it.abb.com>
2018-02-23 18:38:41 +0100

    test(build): add headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <net/if.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
 using namespace std;
 
 int main(){
